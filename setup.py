fe960979e3b081fc66aa19d966b8780ff23bfeb3
SachinBM-CE <sachin.b.manjunath@fau.de>
2023-11-12 19:31:53 +0100

    setup.py editted

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name='math_quiz',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        # list of dependencies here
-    ],
-    entry_points={
-        'console_scripts': [
-            'math_quiz = math_quiz.math_quiz:main',
-        ],
-    },
+    author='Sachin Bharadwaj Manjunath',
+    author_email='sachin.b.manjunath@fau.de',
+    description='DSSS Assignment 2',
+    url='https://github.com/SachinBM-CE/dsss_homework_2.git',
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent']
 )
